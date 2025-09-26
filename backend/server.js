c876a804f374691d67fa4fa4eba04009efffc7ee
anoop-unq <padakantianoop7@gmail.com>
2025-09-26 23:55:53 +0530

    lollll

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ const PORT = process.env.PORT || 3800
 console.log(PORT,"PORT")
 const allowedOrigins = [
     
-    'http://localhost:5173'
-    // 'https://mern-insta-ten.vercel.app'
+    // 'http://localhost:5173'
+    'https://mern-product-amber.vercel.app'
 
 ]
 
